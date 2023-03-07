d240c33cb41efc8d7ac437f278afde5866d94100
aiirononeko <aiirononeko@gmail.com>
2023-03-07 10:56:28 +0900

    til

--- deepDive.ts
+++ deepDive.ts
@@ -57,3 +57,75 @@ const hello = () => {
 let hoge: string = 'hoge';
 
 let fuga: number = 0;
+
+class Point {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  add(point: Point) {
+    return new Point(this.x + point.x, this.y + point.y);
+  }
+}
+
+var p1 = new Point(0, 10);
+var p2 = new Point(10, 20);
+var p3 = p1.add(p2); // {x:10,y:30}
+
+class SomeThing {
+  static instances = 0;
+  constructor() {
+    SomeThing.instances++;
+  }
+}
+
+var s1 = new SomeThing();
+var s2 = new SomeThing();
+console.log(SomeThing.instances); // 2
+
+const iTakeItAll = (first, seconf, ...allOthers) => {
+  console.log(allOthers);
+}
+
+iTakeItAll('foo', 'bar');
+iTakeItAll('foo', 'bar', 'bas', 'qux');
+
+let rect = { x: 0, y: 10, width: 15, height: 20 };
+
+// bunnatudainyuu
+let {x, y, width, height} = rect;
+console.log(x, y, width, height);
+
+rect.x = 10;
+({x, y, width, height} = rect);
+console.log(x, y, width, height);
+
+let someArray = [9, 2, 5];
+for (let index in someArray) {
+  console.log(index); // 0, 1, 2
+}
+
+for (let item of someArray) {
+  console.log(item); // 9, 2, 5 
+}
+
+let hello2: string = "is it me you're looking me?";
+for (let char of hello2) {
+  console.log(char); // is it me you're looking me?
+}
+
+const foo = async () => {
+  try {
+    let val = await getMePromise();
+    console.log(val);
+  } catch(err) {
+    console.log('error');
+  }
+}
+
+const getMePromise = () => {
+  return new Promise.resolve();
+}
+
